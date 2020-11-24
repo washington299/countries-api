9d7bfaab0305ab61117a15954cbc0bea147c0e6e
Washington <washingtoncampos9@gmail.com>
2020-11-24 19:17:40 -0300

    feat: add global styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import GlobalStyles from './globalStyles';
 
 function App() {
   return (
-    <h1>Hello World!</h1>
+		<>
+			<GlobalStyles />
+			<h1>Hello World!</h1>
+		</>
   );
 }
 
