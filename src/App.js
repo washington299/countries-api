81d22d7b21b568b0972f465d425b222b66de0f93
Washington <washingtoncampos9@gmail.com>
2020-11-24 20:39:17 -0300

    chore: put indent as tab instead of spaces

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import GlobalStyles from './globalStyles';
 
-function App() {
-  return (
-		<>
-			<GlobalStyles />
-			<h1>Hello World!</h1>
-		</>
-  );
+class App extends React.Component {
+	render() {
+		return (
+			<>
+				<GlobalStyles />
+				<h1>Hello World!</h1>
+			</>
+		);
+	}
 }
 
 export default App;
