e5653751131c945bfa107e05eb0488522a0ab054
Washington <washingtoncampos9@gmail.com>
2020-11-24 23:44:47 -0300

    feat: create Details page

---
+++ src/pages/details.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Details extends React.Component {
+	render() {
+		return (
+			<h1>Details</h1>
+		);
+	}
+}
+
+export default Details;
--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Home from './pages/home';
+import Details from './pages/details';
 
 class Routers extends React.Component {
 	render() {
 		return (
 			<Switch>
+				<Route path="/details" component={Details} />
 				<Route exact path="/" component={Home} />
 			</Switch>
 		);
