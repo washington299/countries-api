f659e05e528e47eac66d7d0e102d3ee471d7396b
Washington <washingtoncampos9@gmail.com>
2020-11-26 23:53:13 -0300

    feat: change icons and text on click

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,49 @@
 import React from 'react';
+import { WbSunny, NightsStay } from '@material-ui/icons';
 
-import Container from './styles';
+import Container, { Title, ThemeSwitcher } from './styles';
 
 class Header extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { darkMode: false };
+		this.handleClick = this.handleClick.bind(this);
+	}
+
+	componentDidMount() {
+		const theme = localStorage.getItem('theme');
+		if (theme === 'dark') {
+			this.setState({ darkMode: true });
+		} else {
+			this.setState({ darkMode: false });
+		}
+	}
+
+	handleClick() {
+		this.setState((theme) => ({
+			darkMode: !theme.darkMode,
+		}));
+	}
+
 	render() {
+		const { state } = this;
 		return (
-			<Container>Header</Container>
+			<Container>
+				<Title>Where in the world?</Title>
+				<ThemeSwitcher onClick={this.handleClick}>
+					{state.darkMode ? (
+						<>
+							<NightsStay />
+							<span>Dark mode</span>
+						</>
+					) : (
+						<>
+							<WbSunny />
+							<span>Light mode</span>
+						</>
+					)}
+				</ThemeSwitcher>
+			</Container>
 		);
 	}
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -4,5 +4,22 @@ export default styled.header`
 	height: 80px;
 	display: flex;
 	align-items: center;
-	background-color: ${(props) => props.theme.element}
+	justify-content: space-between;
+	background-color: ${(props) => props.theme.element};
+`;
+
+export const Title = styled.strong`
+	font-size: var(--font-size-small);
+	padding-left: 15px;
+`;
+
+export const ThemeSwitcher = styled.div`
+	display: flex;
+	align-items: center;
+	font-size: var(--font-size-small);
+	padding-right: 15px;
+
+	svg {
+		margin-right: 5px;
+	}
 `;
