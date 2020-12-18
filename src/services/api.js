c3a8abcd341cf62d191580eb45eba8f50d3a2350
Washington <washingtoncampos9@gmail.com>
2020-12-18 16:13:08 -0300

    refactor: rename function

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ const api = axios.create({
 	baseURL: 'https://restcountries.eu/rest/v2',
 });
 
-const getAllContries = async () => {
+const getAllCountries = async () => {
 	const res = await api.get('all');
 
 	return res;
@@ -14,7 +14,7 @@ export default async (currentPage, countriesPerPage) => {
 	const startQuantityFrom = (currentPage - 1) * countriesPerPage;
 	const getQuantityUntil = currentPage * 12;
 
-	const countries = await getAllContries();
+	const countries = await getAllCountries();
 	const pagesQuantity = countries.data.length;
 	const res = countries.data.slice(startQuantityFrom, getQuantityUntil);
 	return { res, pagesQuantity };
