d4eb03ab10346a70d96a82a75e28601f8fd1c8e2
Washington <washingtoncampos9@gmail.com>
2020-12-18 12:56:22 -0300

    feat: add activeBackground color

--- src/styles/themes.js
+++ src/styles/themes.js
@@ -4,11 +4,13 @@ export default {
 		background: 'hsl(207, 26%, 17%)',
 		text: 'hsl(0, 0%, 100%)',
 		shadow: 'hsl(207, 26%, 16%)',
+		activeBackground: 'hsl(0, 0%, 98%)',
 	},
 	light: {
 		element: 'hsl(0, 0%, 100%)',
 		background: 'hsl(0, 0%, 98%)',
 		text: 'hsl(200, 15%, 8%)',
 		shadow: 'hsl(0, 0%, 93%)',
+		activeBackground: 'hsl(207, 26%, 17%)',
 	},
 };
