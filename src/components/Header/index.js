efcb8255c7ab8b4d2775b4f4b44864f0c1fdc58d
Washington <washingtoncampos9@gmail.com>
2021-01-17 16:05:32 -0300

    feat: change Header to functional component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,27 +4,11 @@ import ThemeTogglerButton from '../ThemeTogglerButton';
 
 import Container, { Title } from './styles';
 
-class Header extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = { darkMode: false };
-		this.handleClick = this.handleClick.bind(this);
-	}
-
-	handleClick() {
-		this.setState((state) => ({
-			darkMode: !state.darkMode,
-		}));
-	}
-
-	render() {
-		return (
-			<Container>
-				<Title>Where in the world?</Title>
-				<ThemeTogglerButton />
-			</Container>
-		);
-	}
-}
+const Header = () => (
+	<Container>
+		<Title>Where in the world?</Title>
+		<ThemeTogglerButton />
+	</Container>
+);
 
 export default Header;
