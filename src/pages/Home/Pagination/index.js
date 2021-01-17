afa51ce5ec901b5277c25a58f51b819a679ce91b
Washington <washingtoncampos9@gmail.com>
2021-01-17 00:39:13 -0300

    feat: change Pagination to functional component

--- src/pages/Home/Pagination/index.js
+++ src/pages/Home/Pagination/index.js
@@ -1,66 +1,53 @@
-import React from 'react';
-import { withRouter } from 'react-router-dom';
-import PropTypes from 'prop-types';
+import React, { useState, useEffect } from 'react';
 
 import getCountries from '../../../services/api';
-import getQueryStringPage from '../../../helpers/getQueryStringPage';
 
 import Container, { PageNumber } from './styles';
 
-class Pagination extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			pagesQuantity: null,
-			pages: [],
-		};
-	}
+const Pagination = () => {
+	const [pagesQuantity, setPagesQuantity] = useState(null);
+	const pages = [];
+	const currentPage = 1;
 
-	async componentDidMount() {
-		const countriesPerPage = 12;
-		const currentPage = getQueryStringPage(this.props.location.search);
-		const { pagesQuantity } = await getCountries(currentPage, countriesPerPage);
+	useEffect(() => {
+		async function getCountriesFromApi() {
+			const countriesPerPage = 12;
+			const { pagesQuantity: pgQuantity } = await getCountries(1, countriesPerPage);
 
-		for (let i = 1; i <= 6; i += 1) {
-			this.state.pages.push(i);
+			for (let i = 1; i <= 6; i += 1) {
+				pages.push(i);
+			}
+
+			setPagesQuantity(Math.ceil(pgQuantity / countriesPerPage));
 		}
+		getCountriesFromApi();
+	}, []);
 
-		this.setState({ pagesQuantity: Math.ceil(pagesQuantity / countriesPerPage) });
-	}
-
-	render() {
-		const currentPage = getQueryStringPage(this.props.location.search);
-
-		return (
-			<Container>
+	return (
+		<Container>
+			<PageNumber
+				href={`?page=${currentPage - 1}`}
+				style={currentPage === 1 ? { pointerEvents: 'none' } : {}}
+			>
+				&laquo;
+			</PageNumber>
+			{pages.map((page) => (
 				<PageNumber
-					href={`?page=${currentPage - 1}`}
-					style={currentPage === 1 ? { pointerEvents: 'none' } : {}}
+					key={page}
+					active={page === currentPage}
+					href={`?page=${page}`}
 				>
-					&laquo;
+					{page}
 				</PageNumber>
-				{this.state.pages.map((page) => (
-					<PageNumber
-						key={page}
-						active={page === currentPage}
-						href={`?page=${page}`}
-					>
-						{page}
-					</PageNumber>
-				))}
-				<PageNumber
-					href={`?page=${currentPage + 1}`}
-					style={currentPage === this.state.pagesQuantity ? { pointerEvents: 'none' } : {}}
-				>
-					&raquo;
-				</PageNumber>
-			</Container>
-		);
-	}
-}
-
-Pagination.propTypes = {
-	location: PropTypes.object.isRequired,
+			))}
+			<PageNumber
+				href={`?page=${currentPage + 1}`}
+				style={currentPage === pagesQuantity ? { pointerEvents: 'none' } : {}}
+			>
+				&raquo;
+			</PageNumber>
+		</Container>
+	);
 };
 
-export default withRouter(Pagination);
+export default Pagination;
