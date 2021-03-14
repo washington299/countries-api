3f8b94bcdd1308907c6d4206956e8574f9e78163
washington <washingtoncampos9@gmail.com>
2021-03-14 13:14:39 -0300

    refactor: move styled-components ThemeProvider to a separate file

--- src/App.js
+++ src/App.js
@@ -1,40 +1,24 @@
-import React, { useState, useEffect } from "react";
-import { ThemeProvider } from "styled-components";
+import React from "react";
 
 import Routers from "./routes";
 
 import CountriesProvider from "./contexts/countries";
-import ThemeContext from "./contexts/themes";
-import themes from "./styles/themes";
+import ThemeContextProvider from "./contexts/themes";
+
+import StyledComponentsThemeProvider from "./styles/styledComponentsThemeProvider";
 
 import GlobalStyles from "./styles/globalStyles";
 
 const App = () => {
-	const [theme, setTheme] = useState(themes.light);
-
-	function toggleTheme() {
-		setTheme(theme === themes.dark ? themes.light : themes.dark);
-
-		if (theme === themes.dark) {
-			localStorage.setItem("theme", "light");
-		} else {
-			localStorage.setItem("theme", "dark");
-		}
-	}
-
-	useEffect(() => {
-		setTheme(localStorage.getItem("theme") === "light" ? themes.light : themes.dark);
-	}, [theme]);
-
 	return (
-		<ThemeContext.Provider value={{ theme, toggleTheme }}>
-			<CountriesProvider>
-				<ThemeProvider theme={theme}>
+		<ThemeContextProvider>
+			<StyledComponentsThemeProvider>
+				<CountriesProvider>
 					<GlobalStyles />
 					<Routers />
-				</ThemeProvider>
-			</CountriesProvider>
-		</ThemeContext.Provider>
+				</CountriesProvider>
+			</StyledComponentsThemeProvider>
+		</ThemeContextProvider>
 	);
 };
 
